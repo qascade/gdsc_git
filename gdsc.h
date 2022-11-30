5e24f1811ac6606a6f2d268caa8fa640cf060e79
Shubh Karman Singh <54154054+qascade@users.noreply.github.com>
2022-11-30 21:36:35 +0530

    initial commit

---
+++ gdsc.h
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int add(int a, int b);
+
+//New Feature
+float multiply(float a, float b);
---
+++ hello_world.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include "gdsc.h"
+
+
+//Bug
+int add(int a, int b) {
+    return a - b;
+}
+
+
+int main() {
+    puts("Hello World!"); 
+    printf("add(1, 2) = %d", add(1,2));
+    return 0;
+}
